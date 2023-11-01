0df5737ffc8b31a5698b9ef51c4bf9cb02d01270
Tommaso <t.serra996@gmail.com>
2023-11-01 14:00:30 +0100

    Import unit tests for char.py

---
+++ tests/test_char.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest.mock import patch
+import os
+import sys
+import io
+
+# Import the function 'process' from my module
+from assignments.char import process
+
+class TestProcess(unittest.TestCase):
+    def test_test(self):
+        self.assertEqual(2,2)
+
+if __name__=='__main__':
+    unittest.main()
