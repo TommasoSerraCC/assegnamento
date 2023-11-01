e52151962aa8feba3641c1f4cd329879a92fadba
Tommaso <t.serra996@gmail.com>
2023-11-01 17:15:53 +0100

    Error fixed

--- tests/test_charcount.py
+++ tests/test_charcount.py
@@ -5,11 +5,14 @@ import sys
 import io
 
 # Import the function 'process' from my module
-from assignments.charcount import process
+from assignments.char import process
 
 class TestProcess(unittest.TestCase):
     def test_test(self):
         self.assertEqual(2,2)
+
+    def test_test2(self):
+        self.assertEqual(4.,4.)
 
 if __name__=='__main__':
     unittest.main()
